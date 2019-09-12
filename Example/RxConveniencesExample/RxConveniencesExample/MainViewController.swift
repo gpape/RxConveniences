7ee4a129d4578db0a2272a8838cce183b77132b6
Greg Pape <github@gpape.com>
2019-09-12 16:46:11 -0400

    secondary commit

---
+++ Example/RxConveniencesExample/RxConveniencesExample/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  RxConveniencesExample
+//
+//  Created by Greg Pape on 9/11/19.
+//  Copyright © 2019 Greg Pape. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
