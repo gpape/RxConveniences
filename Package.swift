333d65aa88da4d2b257b0ead0e9e884be243ff4b
Greg Pape <github@gpape.com>
2019-09-23 21:17:48 -0400

    fixup

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/gpape/CollectiveSwift.git", from: "2.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxConveniences",
-            dependencies: ["RxCocoa", "RxSwift"]),
+            dependencies: ["CollectiveSwift", "RxCocoa", "RxSwift"]),
         .testTarget(
             name: "RxConveniencesTests",
             dependencies: ["RxConveniences"]),
--- Tests/RxConveniencesTests/RxConveniencesTests.swift
+++ Tests/RxConveniencesTests/RxConveniencesTests.swift
@@ -6,7 +6,8 @@ final class RxConveniencesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RxConveniences().text, "Hello, World!")
+        // TODO:
+        // XCTAssertEqual(RxConveniences().text, "Hello, World!")
     }
 
     static var allTests = [
