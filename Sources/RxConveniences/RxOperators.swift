e0643d702d777270ab4f9a8e78858b05a384c1b1
Greg Pape <github@gpape.com>
2019-09-30 12:14:21 -0400

    feature(logical): add logical operators, tests

--- Sources/RxConveniences/RxOperators.swift
+++ Sources/RxConveniences/RxOperators.swift
@@ -27,6 +27,16 @@ import RxSwift
 
 extension ObservableType where Element == Bool {
 
+    /// Applies a logical AND to two boolean `Observable`s via `combineLatest`.
+    public static func && (left: Self, right: Self) -> Observable<Bool> {
+        return Observable.combineLatest(left, right).map { $0.0 && $0.1 }
+    }
+
+    /// Applies a logical OR to two boolean `Observable`s via `combineLatest`.
+    public static func || (left: Self, right: Self) -> Observable<Bool> {
+        return Observable.combineLatest(left, right).map { $0.0 || $0.1 }
+    }
+
     /// Apply logical negation to a boolean observable.
     public static prefix func ! (observable: Self) -> Observable<Bool> {
         return observable.map { !$0 }
@@ -36,8 +46,18 @@ extension ObservableType where Element == Bool {
 
 extension SharedSequence where Element == Bool {
 
+    /// Applies a logical AND to two boolean `SharedSequence`s via `combineLatest`.
+    public static func && (left: Self, right: Self) -> Self {
+        return .combineLatest(left, right) { $0 && $1 }
+    }
+
+    /// Applies a logical OR to two boolean `SharedSequence`s via `combineLatest`.
+    public static func || (left: Self, right: Self) -> Self {
+        return .combineLatest(left, right) { $0 || $1 }
+    }
+
     /// Apply logical negation to a boolean shared sequence.
-    public static prefix func ! (observable: SharedSequence<SharingStrategy, Element>) -> SharedSequence<SharingStrategy, Element> {
+    public static prefix func ! (observable: Self) -> Self {
         return observable.map { !$0 }
     }
 
--- Tests/RxConveniencesTests/RxConveniencesTests.swift
+++ Tests/RxConveniencesTests/RxConveniencesTests.swift
@@ -1,16 +1,74 @@
+import RxTest
 import XCTest
 @testable import RxConveniences
 
 final class RxConveniencesTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        // TODO:
-        // XCTAssertEqual(RxConveniences().text, "Hello, World!")
+
+    func testLogicalAND() {
+
+        let scheduler = TestScheduler(initialClock: 0)
+
+        let a = scheduler.createHotObservable([
+            .next(100, true),
+            .next(400, false),
+            .next(500, true)
+        ])
+
+        let b = scheduler.createHotObservable([
+            .next(300, true),
+            .next(600, false),
+            .next(700, true)
+        ])
+
+        // subscribed at 200
+        // disposed at 1000
+        let results = scheduler.start { a && b }
+
+        let correctEvents = Recorded.events(
+            .next(400, false),
+            .next(500, true),
+            .next(600, false),
+            .next(700, true)
+        )
+
+        XCTAssertEqual(results.events, correctEvents)
+
+    }
+
+    func testLogicalOR() {
+
+        let scheduler = TestScheduler(initialClock: 0)
+
+        let a = scheduler.createHotObservable([
+            .next(100, true),
+            .next(400, false),
+            .next(500, true)
+        ])
+
+        let b = scheduler.createHotObservable([
+            .next(300, true),
+            .next(600, false),
+            .next(700, true)
+        ])
+
+        // subscribed at 200
+        // disposed at 1000
+        let results = scheduler.start { a || b }
+
+        let correctEvents = Recorded.events(
+            .next(400, true),
+            .next(500, true),
+            .next(600, true),
+            .next(700, true)
+        )
+
+        XCTAssertEqual(results.events, correctEvents)
+
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testLogicalAND", testLogicalAND),
+        ("testLogicalOR", testLogicalOR)
     ]
+
 }
