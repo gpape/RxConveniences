baaec489be1959b5ef3b9687db31aa786f48726e
Greg Pape <github@gpape.com>
2020-11-27 11:57:39 -0500

    checkpoint

--- iOS Example/iOS Example/View Controllers/Showreel/ShowreelViewController.swift
+++ iOS Example/iOS Example/View Controllers/Showreel/ShowreelViewController.swift
@@ -25,40 +25,42 @@ private extension ShowreelViewController {
 
     private func create(at position: Showreel.Position) {
 
-        let anchor = UIView()
-        anchor.translatesAutoresizingMaskIntoConstraints = false
-        anchorsContainer.addSubview(anchor)
+        let newView = ShowreelViewFactory.makeView()
+        newView.alpha = 0
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newView)
 
-        let y = anchor.centerYAnchor.constraint(equalTo: anchorsContainer.centerYAnchor, constant: position.y)
-
-        NSLayoutConstraint.activate([
-            anchor.widthAnchor.constraint(equalToConstant: 1),
-            anchor.heightAnchor.constraint(equalToConstant: 1),
-            anchor.centerXAnchor.constraint(equalTo: anchorsContainer.centerXAnchor, constant: position.x),
-            y
-        ])
-
-        let control = UISwitch()
-        control.isOn = true
-        control.translatesAutoresizingMaskIntoConstraints = false
-        canvas.addSubview(control)
-
-        control.layer.zPosition = position.z
         var transform = CATransform3DIdentity
         transform.m34 = -1.0 / 250
         transform = CATransform3DTranslate(transform, 0, 0, position.z)
-        control.layer.transform = transform
+        newView.layer.transform = transform
+        newView.layer.zPosition = position.z
+
+        let y = newView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: position.y)
 
         NSLayoutConstraint.activate([
-            control.centerXAnchor.constraint(equalTo: anchor.centerXAnchor),
-            control.centerYAnchor.constraint(equalTo: anchor.centerYAnchor)
+            newView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: position.x),
+            y
         ])
 
         DispatchQueue.main.async {
+
             y.constant -= UIScreen.height
+
+            UIView.animate(withDuration: 0.3, delay: 0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
+                newView.alpha = 1
+            }, completion: nil)
+
             UIView.animate(withDuration: 10, delay: 0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
                 self.view.layoutIfNeeded()
             }, completion: nil)
+
+//            UIView.animate(withDuration: 0.3, delay: 9.7, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
+//                newView.alpha = 0
+//            }, completion: { _ in
+//                newView.removeFromSuperview()
+//            })
+
         }
 
     }
@@ -68,13 +70,6 @@ private extension ShowreelViewController {
 // MARK: -
 
 final class ShowreelViewController: UIViewController {
-
-// MARK: - Interface
-
-    @IBOutlet private weak var anchorsContainer: UIView!
-    @IBOutlet private weak var canvas: UIView!
-
-// MARK: -
 
     private let bag = DisposeBag()
     private let vm = ShowreelViewModel()
---
+++ iOS Example/iOS Example/View Controllers/Showreel/ShowreelViewFactory.swift
@@ -0,0 +1,65 @@
+//
+//  ShowreelViewFactory.swift
+//  iOS Example
+//
+//  Created by Greg Pape on 11/27/20.
+//
+
+import UIKit
+
+enum ShowreelViewFactory {
+
+    static func makeView() -> UIView {
+        ViewType.allCases.randomElement()!.makeView()
+    }
+
+}
+
+// MARK: - Types
+
+private enum ViewType: CaseIterable {
+
+    case activityIndicator
+    case slider
+    case `switch`
+    case symbol
+
+    func makeView() -> UIView {
+
+        switch self {
+        case .activityIndicator:
+
+            let view = UIActivityIndicatorView(style: .large)
+            view.startAnimating()
+            return view
+
+        case .slider:
+
+            let view = UISlider()
+            view.value = .random(in: 0.333..<0.667)
+            NSLayoutConstraint.activate([view.widthAnchor.constraint(equalToConstant: 44)])
+            return view
+
+        case .switch:
+
+            let view = UISwitch()
+            view.isOn = true
+            return view
+
+        case .symbol:
+
+            return UIImageView(image: UIImage(named: names.randomElement()!))
+
+        }
+
+    }
+
+}
+
+private let names = [
+    "folder.badge.plus",
+    "pencil.circle",
+    "square.and.arrow.up",
+    "trash",
+    "tray.and.arrow.down.fill"
+]
